6d449864763571c613b0f05bcc8661c77b1e43d2
Jhanina Romero <jhanina.mel.9@gmail.com>
2021-10-18 23:07:07 -0400

    Created method edi and remove

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 import "./App.css";
 import { StudentService } from "./service/StudentService";
 import { DataTable } from "primereact/datatable";
@@ -7,6 +8,7 @@ import { Panel } from "primereact/panel";
 import { Menubar } from "primereact/menubar";
 import { Dialog } from "primereact/dialog";
 import { InputText } from "primereact/inputtext";
+import {Button} from "primereact/button"
 
 import "primereact/resources/themes/saga-blue/theme.css";
 import "primereact/resources/primereact.min.css";
@@ -14,8 +16,20 @@ import "primeicons/primeicons.css";
 
 export default class App extends Component {
   constructor() {
+    
     super();
-    this.state = {};
+    this.state = {
+      visible: false,
+      visible2:false,
+      student: {
+        name: "",
+        email: "",
+        dob: "",
+      },
+      selectedStudent:{ 
+
+      }
+    };
     this.items = [
       {
         label: "New",
@@ -25,18 +39,15 @@ export default class App extends Component {
       {
         label: "Edit",
         icon: "pi pi-fw pi-pencil",
-        command: () => {
-          alert("Updated");
-        },
+        command: () => {this.showUpdateDialog()    },
       },
       {
         label: "Delete",
         icon: "pi pi-fw pi-trash",
-        command: () => {
-          alert("Deleted");
-        },
+        command: () => {this.eliminar()},
       },
     ];
+    
     this.studentService = new StudentService();
   }
 
@@ -44,25 +55,40 @@ export default class App extends Component {
     this.studentService
       .getAll()
       .then((data) => this.setState({ students: data }));
-    this.setState({
-      visible: false,
-      student: {
-        id: null,
-        name: null,
-        email: null,
-        dob: null,
-        age: null,
-      },
-    });
+    
   }
+  guardar = () => {
+   this.studentService.registerNewStudents(this.state.student).then((response) => {
+        console.log(response.data)
+    });
+    // StudentService.getAll().then((response) => {
+    //     this.setState({ students: response.data })
+    // });
+    this.setState({ visible: false });
+}
+editar = () => {
+  this.studentService.updateStudents(this.state.student.id, this.state.student.name, this.state.student.email)
+  this.setState({ visible2: false });
+  console.log(this.state)
+};
+eliminar = (id) => {
+  var opcion = window.confirm("Estás Seguro que deseas Eliminar al estudiante " +this.state.selectedStudent.name);
+  if (opcion === true) {
+      this.studentService.deleteStudents(this.state.selectedStudent.id).then((response) => {
+          console.log(response.data)
 
+      });
+  }
+};
+
+  
   render() {
     return (
       <div style={{ width: "80%", marginTop: "20px", margin: "0 auto" }}>
         <Menubar model={this.items} />
         <br />
         <Panel header="Students">
-          <DataTable value={this.state.students}>
+          <DataTable value={this.state.students} selectionMode="single" selection={this.state.selectedStudent} onSelectionChange={e => this.setState({selectedStudent:e.value})}>
             <Column field="id" header="ID"></Column>
             <Column field="name" header="Name"></Column>
             <Column field="email" header="Email"></Column>
@@ -78,7 +104,7 @@ export default class App extends Component {
           onHide={() => this.setState({ visible: false })}
         >
           <span className="p-float-label" id="name">
-          <InputText value={this.state.value} onChange={(e) => this.setState(prevState => {
+          <InputText type="text" value={this.state.student.name} onChange={(e) => this.setState(prevState => {
             let student = Object.assign({}, prevState.student)
             student.name = e.target.value
 
@@ -87,7 +113,7 @@ export default class App extends Component {
           <label htmlFor="name">NAME</label>
           </span>
           <span className="p-float-label" id="email">
-          <InputText value={this.state.value} onChange={(e) => this.setState(prevState => {
+          <InputText type="email" value={this.state.student.email} onChange={(e) => this.setState(prevState => {
             let student = Object.assign({}, prevState.student)
             student.email = e.target.value
 
@@ -96,14 +122,49 @@ export default class App extends Component {
           <label htmlFor="email">EMAIL</label>
           </span>
           <span className="p-float-label" id="dob">
-          <InputText value={this.state.value} onChange={(e) => this.setState(prevState => {
+          <InputText type="date" value={this.state.student.dob} onChange={(e) => this.setState(prevState => {
             let student = Object.assign({}, prevState.student)
             student.dob = e.target.value
 
             return { student };
           })}/>
-          <label htmlFor="email">DATE OF BIRTH</label>
           </span>
+          <Button  onClick={()=>this.guardar()}> guardar</Button>
+        </Dialog>
+        <Dialog
+          header="Edit Student"
+          visible={this.state.visible2}
+          style={{ width: "400px" }}
+          modal={true}
+          onHide={() => this.setState({ visible2: false })}
+        >
+          <span className="p-float-label" id="name">
+          <InputText type="text" value={this.state.student.name} onChange={(e) => this.setState(prevState => {
+            let student = Object.assign({}, prevState.student)
+            student.name = e.target.value
+
+            return { student };
+          })}/>
+          <label htmlFor="name">NAME</label>
+          </span>
+          <span className="p-float-label" id="email">
+          <InputText type="email" value={this.state.student.email} onChange={(e) => this.setState(prevState => {
+            let student = Object.assign({}, prevState.student)
+            student.email = e.target.value
+
+            return { student };
+          })}/>
+          {/* <label htmlFor="email">EMAIL</label> */}
+          </span>
+          {/* <span className="p-float-label" id="dob">
+          <InputText type="date" value={this.state.student.dob} onChange={(e) => this.setState(prevState => {
+            let student = Object.assign({}, prevState.student)
+            student.dob = e.target.value
+
+            return { student };
+          })}/>
+          </span> */}
+          <Button  onClick={()=>this.editar()}> guardar</Button>
         </Dialog>
       </div>
     );
@@ -112,5 +173,15 @@ export default class App extends Component {
     this.setState({
       visible: true,
     });
+  }
+  showUpdateDialog(){
+    this.setState({
+      visible2:true,
+      student: {
+        id: this.state.selectedStudent.id,
+        name: this.state.selectedStudent.name,
+        email: this.state.selectedStudent.email
+      },
+    })
   }
 }
--- react-app/src/service/StudentService.js
+++ react-app/src/service/StudentService.js
@@ -1,8 +1,20 @@
 import axios from 'axios'
 
 export class StudentService{
-    baseURL = "http://localhost:8080/api/v1/";
+    baseURL = "http://localhost:8080/api/v1/student";
     getAll(){
-        return axios.get(this.baseURL + "student").then(res => res.data);
+        return axios.get(this.baseURL).then(res => res.data);
     }
+    registerNewStudents(student){
+        return axios.post(this.baseURL, student);
+    }
+    updateStudents(studentId, name, email){
+        return axios.put(this.baseURL+'/'+ studentId+'?name='+name+'&email='+email)
+    }
+    deleteStudents(studentId){
+        return axios.delete(this.baseURL+'/'+ studentId);
+    }
+
+
+
 }
